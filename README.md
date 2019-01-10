689d405b367ff9f61dceab64e006372a84fead6f
Georgi Yanev <georgiyanev.gy@gmail.com>
2019-01-10 23:58:52 +0200

    Create the toggle emoji component

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # react-toggle-emoji
+
+A small library of react toggle button components that use emoji to express themselves.
+
+<img src="./react-toggle-emoji-preview.jpg" alt="preview of react-toggle-emoji components configured in different ways using props" />
+
+## Social Links
+
+- [www.georgi-yanev.com](https://www.georgi-yanev.com)
+- [blog.georgi-yanev.com](https://blog.georgi-yanev.com)
+- [Facebook](https://www.facebook.com/jumpalottahigh/)
+- [Twitter](https://www.twitter.com/jumpalottahigh/)
+- [LinkedIn](https://www.linkedin.com/in/yanevgeorgi/)
--- src/index.js
+++ src/index.js
@@ -1,20 +1,109 @@
 import React from 'react'
+import styled from 'styled-components'
 
-const style = {
-  border: 'none',
-  width: '60px',
-  height: '60px',
-  fontSize: '32px',
-  display: 'flex',
-  justifyContent: 'center',
-  alignItems: 'center',
-  lineHeight: '1rem',
-  backgroundColor: '#d2d2d2',
-  cursor: 'pointer',
-  borderRadius: '50%',
-  boxShadow:
-    '0px 7px 18px rgba(0, 0, 0, 0.25), 0px 7px 10px rgba(0, 0, 0, 0.22)'
+const sizes = {
+  small: {
+    width: 48,
+    height: 48,
+    fontSize: 28
+  },
+  default: {
+    width: 64,
+    height: 64,
+    fontSize: 36
+  },
+  large: {
+    width: 80,
+    height: 80,
+    fontSize: 48
+  }
 }
+
+const emojiList = {
+  book: {
+    open: '📖',
+    close: '📘'
+  },
+  glasses: {
+    open: '😍',
+    close: '😎'
+  },
+  laugh: {
+    open: '🤣',
+    close: '😂'
+  },
+  horse: {
+    open: '🦄',
+    close: '🐴'
+  },
+  skull: {
+    open: '☠',
+    close: '💀'
+  },
+
+  hand: {
+    open: '👍',
+    close: '👎'
+  },
+  shoe: {
+    open: '👠',
+    close: '👡'
+  },
+  sound: {
+    open: '🔊',
+    close: '🔈'
+  },
+  mailbox: {
+    open: '📭',
+    close: '📫'
+  },
+  moon: {
+    open: '🌕',
+    close: '🌑'
+  },
+  fire: {
+    open: '🔥',
+    close: '💧'
+  },
+  arrow: {
+    open: '⬇',
+    close: '⬆'
+  }
+}
+
+const Button = styled.button`
+  cursor: pointer;
+  transition: 125ms;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: ${sizes.default.width}px;
+  height: ${sizes.default.height}px;
+  font-size: ${sizes.default.fontSize}px;
+  background-color: ${props => (props.bgColor ? props.bgColor : '#fff')};
+  box-shadow: ${props =>
+    props.noShadow
+      ? 'none'
+      : 'rgba(0, 0, 0, 0.2) 0px 1px 5px 0px, rgba(0, 0, 0, 0.14) 0px 2px 2px 0px, rgba(0, 0, 0, 0.12) 0px 3px 1px -2px;'};
+  border-radius: ${props => (props.borderRadius ? props.borderRadius : '50%')};
+  border: ${props => (props.border ? props.border : 'none')};
+
+  &:hover {
+    box-shadow: 0 3px 6px rgba(0, 0, 0, 0.16), 0 3px 6px rgba(0, 0, 0, 0.23);
+  }
+
+  &.small {
+    width: ${sizes.small.width}px;
+    height: ${sizes.small.height}px;
+    font-size: ${sizes.small.fontSize}px;
+  }
+
+  &.large {
+    width: ${sizes.large.width}px;
+    height: ${sizes.large.height}px;
+    font-size: ${sizes.large.fontSize}px;
+  }
+`
 
 export default class ToggleEmoji extends React.Component {
   constructor(props) {
@@ -24,15 +113,33 @@ export default class ToggleEmoji extends React.Component {
 
   render() {
     const { open } = this.state
+    const { size, emojiSet, customEmojiOpen, customEmojiClose } = this.props
+
+    let emojiOpen = emojiList.book.open
+    let emojiClose = emojiList.book.close
+
+    // Set emoji based on emojiSet
+    if (Object.keys(emojiList).includes(emojiSet)) {
+      emojiOpen = emojiList[emojiSet].open
+      emojiClose = emojiList[emojiSet].close
+    }
+
+    // Set emoji based on custom emoji
+    if (customEmojiOpen && customEmojiClose) {
+      emojiOpen = customEmojiOpen
+      emojiClose = customEmojiClose
+    }
+
     return (
-      <button
-        style={style}
+      <Button
+        {...this.props}
+        className={size}
         onClick={() => {
           this.setState(prev => ({ open: !prev.open }))
         }}
       >
-        {open ? '📖' : '📘'}
-      </button>
+        {open ? emojiOpen : emojiClose}
+      </Button>
     )
   }
 }
